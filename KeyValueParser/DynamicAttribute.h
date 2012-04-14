741f04662705c977d9e46c97b48c948eef216dd8
Diego Chohfi <dchohfi@gmail.com>
2012-04-14 14:48:50 -0300

    adding Dynamic Attribute type, for information about the attribute on object adding tests for DynamicAttribute refactoring to use the new class

---
+++ KeyValueParser/DynamicAttribute.h
@@ -0,0 +1,20 @@
+//
+//  DynamicAttribute.h
+//  KeyValueParser
+//
+//  Created by Diego Chohfi on 4/14/12.
+//  Copyright (c) 2012 None. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DynamicAttribute : NSObject
+
+@property(nonatomic, readonly) NSString *attributeType;
+@property(nonatomic, readonly) NSString *attributeName;
+@property(nonatomic, readonly, getter = isPrimitive) BOOL primitive;
+@property(nonatomic, readonly, getter = isIdType) BOOL idType;
+
+- (id)initWithAttributeDescription: (NSString *) description;
+- (Class) attributeClass;
+@end
--- KeyValueParser/KeyValueParser.m
+++ KeyValueParser/KeyValueParser.m
@@ -8,6 +8,7 @@
 
 #import "KeyValueParser.h"
 #import "GenericParser.h"
+#import "DynamicAttribute.h"
 #import <objc/runtime.h>
 
 @interface KeyValueParser()
@@ -32,12 +33,12 @@
         id value = [json valueForKey:key];
         objc_property_t property = class_getProperty(class, [key UTF8String]);
         if (property) {
-            NSString *className = [NSString stringWithUTF8String:getPropertyType(property)];
-            Class propertyClass = NSClassFromString(className);
+            NSString *attributeDetails = [NSString stringWithUTF8String:property_getAttributes(property)];
+            DynamicAttribute *dynamicAttribute = [[DynamicAttribute alloc] initWithAttributeDescription: attributeDetails];
             if([value isKindOfClass:[NSDictionary class]]){
-                value = [self parseJson:(NSDictionary *) value forClass:propertyClass];
+                value = [self parseJson:(NSDictionary *) value forClass:[dynamicAttribute attributeClass]];
             }
-            value = [parser transformValue:value forClass:propertyClass withConfiguration:configuration];
+            value = [parser transformValue:value forClass:[dynamicAttribute attributeClass] withConfiguration:configuration];
             NSError *error;
             if([object validateValue:&value forKey:key error:&error]){
                 [object setValue:value forKey:key];
@@ -45,25 +46,6 @@
         }
     }
     return object;
-}
-
-static const char *getPropertyType(objc_property_t property) {
-    const char *attributes = property_getAttributes(property);
-    char buffer[1 + strlen(attributes)];
-    strcpy(buffer, attributes);
-    char *state = buffer, *attribute;
-    while ((attribute = strsep(&state, ",")) != NULL) {
-        if (attribute[0] == 'T' && attribute[1] != '@') {
-            return (const char *)[[NSData dataWithBytes:(attribute + 1) length:strlen(attribute) - 1] bytes];
-        }        
-        else if (attribute[0] == 'T' && attribute[1] == '@' && strlen(attribute) == 2) {
-            return "id";
-        }
-        else if (attribute[0] == 'T' && attribute[1] == '@') {
-            return (const char *)[[NSData dataWithBytes:(attribute + 3) length:strlen(attribute) - 4] bytes];
-        }
-    }
-    return "";
 }
 
 @end
