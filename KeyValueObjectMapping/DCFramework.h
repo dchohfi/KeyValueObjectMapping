9950a9d905d088aa26a3e2dfe08e1c1d35171117
Diego Chohfi <dchohfi@gmail.com>
2012-04-18 11:33:17 -0300

    Adding framework capabilities

---
+++ KeyValueObjectMapping/DCFramework.h
@@ -0,0 +1,12 @@
+//
+//  DCFramework.h
+//  KeyValueObjectMapping
+//
+//  Created by Diego Chohfi on 4/18/12.
+//  Copyright (c) 2012 dchohfi. All rights reserved.
+//
+
+#import "DCKeyValueObjectMapping.h"
+#import "DCParserConfiguration.m"
+#import "DCObjectMapping.h"
+#import "DCArrayMapping.h"
