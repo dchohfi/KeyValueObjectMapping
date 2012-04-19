8ccd8cf007901401788a263d0fce792dd5e93557
Diego Chohfi <dchohfi@gmail.com>
2012-04-19 20:57:33 -0300

    more tests

--- KeyValueObjectMapping/DCDictionaryRearranger.m
+++ KeyValueObjectMapping/DCDictionaryRearranger.m
@@ -13,12 +13,13 @@
 
 
 + (NSDictionary *) rearrangeDictionary: (NSDictionary *) dictionary forAggregators: (NSMutableArray *) aggregators {
+    aggregators = [NSMutableArray arrayWithArray:[[aggregators reverseObjectEnumerator] allObjects]];
     NSMutableDictionary *mutableDictionary = [NSMutableDictionary dictionaryWithDictionary:dictionary];
     if(aggregators && [aggregators count] > 0){
         for(int i=[aggregators count] - 1; i >= 0; --i){
             DCPropertyAggregator *aggretor = [aggregators objectAtIndex:i];
             [aggregators removeObject:aggretor];
-            NSDictionary *aggregatedValues = [aggretor aggregateKeysOnDictionary:dictionary];
+            NSDictionary *aggregatedValues = [aggretor aggregateKeysOnDictionary:mutableDictionary];
             [mutableDictionary setValue:aggregatedValues forKey:aggretor.attribute];
         }
     }
--- KeyValueObjectMappingTests/DCPropertyAggregatorTests.m
+++ KeyValueObjectMappingTests/DCPropertyAggregatorTests.m
@@ -9,6 +9,7 @@
 #import "DCPropertyAggregatorTests.h"
 #import "DCKeyValueObjectMapping.h"
 #import "DCParserConfiguration.h"
+#import "DCDictionaryRearranger.h"
 #import "Bus.h"
 @implementation DCPropertyAggregatorTests
 
@@ -30,8 +31,31 @@
     Bus *bus = [parser parseDictionary:dictionaryToParse];
     STAssertNotNil(bus.location, @"Should be able to create a location using aggregator");
     STAssertEqualObjects(bus.name, busName, @"Should be equals");
-    STAssertEquals(bus.location.latitude, latitude, @"Should be equals");
-    STAssertEquals(bus.location.longitude, longitude, @"Should be equals");
+    STAssertEqualObjects(bus.location.latitude, latitude, @"Should be equals");
+    STAssertEqualObjects(bus.location.longitude, longitude, @"Should be equals");
+}
+
+- (void) testAggregateMultipleRules {
+    NSString *busName = @"R. Dr. Neto De Araujo, 311";
+    NSNumber *latitude = [NSNumber numberWithInt:-123];
+    NSNumber *longitude = [NSNumber numberWithInt:-321];
+    NSNumber *distance = [NSNumber numberWithInt:100];
+    NSMutableDictionary *dictionaryToParse = [[NSMutableDictionary alloc] init];
+    [dictionaryToParse setObject:busName forKey:@"name"];
+    [dictionaryToParse setObject:latitude forKey:@"latitude"];
+    [dictionaryToParse setObject:longitude forKey:@"longitude"];
+    [dictionaryToParse setObject:distance forKey:@"distance"];
+
+    DCPropertyAggregator *aggregteLatLong = [DCPropertyAggregator aggregateKeys:[NSSet setWithObjects:@"latitude", @"longitude", nil] intoAttribute:@"point"];
+
+    DCPropertyAggregator *aggregatePointDist = [DCPropertyAggregator aggregateKeys:[NSSet setWithObjects:@"point", @"distance", nil] intoAttribute:@"location"];
+
+    NSDictionary *aggregatedDict = [DCDictionaryRearranger rearrangeDictionary:dictionaryToParse forAggregators:[NSArray arrayWithObjects:aggregteLatLong, aggregatePointDist, nil]];
+    STAssertNotNil(aggregatedDict, @"Should be able to create a location using aggregator");
+    STAssertEqualObjects([aggregatedDict objectForKey:@"name"], busName, @"Should be equals");
+    STAssertEqualObjects([[aggregatedDict objectForKey:@"location"] objectForKey:@"distance"], distance, @"Should be equals");
+    STAssertEqualObjects([[[aggregatedDict objectForKey:@"location"] objectForKey:@"point"] objectForKey:@"latitude"], latitude, @"Should be equals");
+    STAssertEqualObjects([[[aggregatedDict objectForKey:@"location"] objectForKey:@"point"] objectForKey:@"longitude"], longitude, @"Should be equals");
 }
 
 
