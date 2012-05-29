725ae86ad6ca3d6b5f2f5bcc6c90bbb3996a5a98
Sergey Klimov <sergey.v.klimov@gmail.com>
2012-05-29 13:19:44 -0500

    closed #6

---
+++ KeyValueObjectMappingTests/Album.m
@@ -0,0 +1,19 @@
+//
+//  Album.m
+//  KeyValueObjectMapping
+//
+//  Created by Sergey Klimov on 5/29/12.
+//  Copyright (c) 2012 dchohfi. All rights reserved.
+//
+
+#import "Album.h"
+#import "Song.h"
+
+
+@implementation Album
+
+@dynamic id;
+@dynamic name;
+@dynamic songs;
+
+@end
---
+++ KeyValueObjectMappingTests/Artist.m
@@ -0,0 +1,19 @@
+//
+//  Artist.m
+//  KeyValueObjectMapping
+//
+//  Created by Sergey Klimov on 5/29/12.
+//  Copyright (c) 2012 dchohfi. All rights reserved.
+//
+
+#import "Artist.h"
+#import "Song.h"
+
+
+@implementation Artist
+
+@dynamic id;
+@dynamic name;
+@dynamic songs;
+
+@end
